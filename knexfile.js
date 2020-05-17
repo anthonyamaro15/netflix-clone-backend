738d694efa1e68befd92f9dcea06a4ec1608317e
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-16 22:51:52 -0700

    test

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,33 @@
 module.exports = {
-  //   development: {
-  //     client: "pg",
-
-  //     connection: "postgres://localhost/users",
-  //     migrations: {
-  //       directory: "./data/migrations",
-  //     },
-  //     seeds: {
-  //       directory: "./data/seeds",
-  //     },
-  //   },
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/users.db3",
-    },
+    client: "pg",
+
+    connection: "postgres://localhost/users",
     migrations: {
       directory: "./data/migrations",
     },
     seeds: {
       directory: "./data/seeds",
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
   },
+  //   development: {
+  //     client: "sqlite3",
+  //     useNullAsDefault: true,
+  //     connection: {
+  //       filename: "./data/users.db3",
+  //     },
+  //     migrations: {
+  //       directory: "./data/migrations",
+  //     },
+  //     seeds: {
+  //       directory: "./data/seeds",
+  //     },
+  //     pool: {
+  //       afterCreate: (conn, done) => {
+  //         conn.run("PRAGMA foreign_keys = ON", done);
+  //       },
+  //     },
+  //   },
 
   production: {
     client: "pg",
